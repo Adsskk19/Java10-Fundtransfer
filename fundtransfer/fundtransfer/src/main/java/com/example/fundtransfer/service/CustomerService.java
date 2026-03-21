ea76dd750a3af57a0eb324a965eff98655697c3a
Adsskk19 <saikiranankam19@gmail.com>
2026-03-21 16:16:36 +0530

    Customer service Implementation

--- fundtransfer/fundtransfer/src/main/java/com/example/fundtransfer/service/CustomerService.java
+++ fundtransfer/fundtransfer/src/main/java/com/example/fundtransfer/service/CustomerService.java
@@ -8,32 +8,18 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class CustomerService {
+public interface CustomerService {
 
     @Autowired
-    private CustomerRepository repository;
+    CustomerRepository repository;
 
-    public Customer saveCustomer(Customer customer) {
-        return repository.save(customer);
-    }
+    public Customer saveCustomer(Customer customer);
 
-    public List<Customer> getAllCustomers() {
-        return repository.findAll();
-    }
+    public List<Customer> getAllCustomers() ;
 
-    public Customer getCustomerById(Long id) {
-        return repository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Customer not found"));
-    }
+    public Customer getCustomerById(Long id);
 
-    public Customer updateCustomer(Long id, Customer updatedCustomer) {
-        Customer existing = getCustomerById(id);
-        existing.setName(updatedCustomer.getName());
-        existing.setPassword(updatedCustomer.getPassword());
-        return repository.save(existing);
-    }
+    public Customer updateCustomer(Long id, Customer updatedCustomer);
 
-    public void deleteCustomer(Long id) {
-        repository.deleteById(id);
-    }
+
 }
---
+++ fundtransfer/fundtransfer/src/main/java/com/example/fundtransfer/service/CustomerServiceImpl.java
@@ -0,0 +1,37 @@
+package com.example.fundtransfer.service;
+
+import com.example.fundtransfer.entity.Customer;
+import com.example.fundtransfer.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+public class CustomerServiceImpl implements CustomerService {
+
+
+    @Autowired
+    private CustomerRepository repository;
+
+    public Customer saveCustomer(Customer customer) {
+        return repository.save(customer);
+    }
+
+    public List<Customer> getAllCustomers() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Customer getCustomerById(Long id) {
+        return repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Customer not found"));
+    }
+
+    @Override
+    public Customer updateCustomer(Long id, Customer updatedCustomer) {
+        Customer existing = getCustomerById(id);
+        existing.setName(updatedCustomer.getName());
+        existing.setPassword(updatedCustomer.getPassword());
+        return repository.save(existing);
+    }
+}
