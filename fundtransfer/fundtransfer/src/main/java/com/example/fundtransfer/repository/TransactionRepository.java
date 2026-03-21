9ae882ccb3f21828c0df89202e2c4196ef04e4d0
ram dinesh reddy <ramdinu6971@gmail.com>
2026-03-21 16:01:55 +0530

    Repo

--- fundtransfer/fundtransfer/src/main/java/com/example/fundtransfer/repository/TransactionRepository.java
+++ fundtransfer/fundtransfer/src/main/java/com/example/fundtransfer/repository/TransactionRepository.java
@@ -2,9 +2,11 @@ package com.example.fundtransfer.repository;
 
 import com.example.fundtransfer.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     List<Transaction> findByAccountId(Long accountId);
